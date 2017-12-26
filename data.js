5f0fd9d96a45abad7e729e75f562f6bb09cc97ff
William Lim <william.lim@terasquad.com>
2017-12-26 17:30:58 +0800

    Initial Commit

---
+++ data.js
@@ -0,0 +1,101 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20171226_152914",
+      "name": "IMG_20171226_152914",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 3.0859204178729147,
+        "pitch": 0.3691047655031774,
+        "fov": 1.283603139734972
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.119473946204348,
+          "pitch": 0.5458315664653046,
+          "rotation": 0,
+          "target": "1-img_20171226_152921"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0859204178729147,
+          "pitch": 0.3691047655031774,
+          "title": "Kitchen",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20171226_152921",
+      "name": "IMG_20171226_152921",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.07927956045189433,
+        "pitch": -0.047429352328894225,
+        "fov": 1.283603139734972
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06431062697900813,
+          "pitch": 0.37641012626031234,
+          "rotation": 0,
+          "target": "0-img_20171226_152914"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.005587880923247468,
+          "pitch": 0.3575103288489281,
+          "title": "Bedroom",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "William Bedroom",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

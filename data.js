aa6e3765ce67926e62c346aa89239fc3ac1a3c5b
William Lim <william.lim@terasquad.com>
2018-03-09 16:41:48 +0800

    Kanvas Soho

--- data.js
+++ data.js
@@ -1,7 +1,45 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-bedroom",
+      "id": "0-living-room",
+      "name": "Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5411290492867895,
+          "pitch": 0.37814682850036263,
+          "rotation": 0,
+          "target": "1-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bedroom",
       "name": "Bedroom",
       "levels": [
         {
@@ -24,30 +62,23 @@ var APP_DATA = {
       ],
       "faceSize": 1728,
       "initialViewParameters": {
-        "yaw": -0.019040359802373175,
-        "pitch": 0.08538990843730332,
-        "fov": 1.283603139734972
+        "yaw": -1.5708200091826772,
+        "pitch": 0.18445223227037744,
+        "fov": 1.285659589108446
       },
       "linkHotspots": [
         {
-          "yaw": 3.118771572807761,
-          "pitch": 0.28444648066619393,
+          "yaw": 1.6171668600566118,
+          "pitch": 0.3133943650317619,
           "rotation": 0,
-          "target": "1-kitchen"
+          "target": "0-living-room"
         }
       ],
-      "infoHotspots": [
-        {
-          "yaw": 3.12620545699437,
-          "pitch": 0.5881267119879823,
-          "title": "Kitchen",
-          "text": "Text"
-        }
-      ]
+      "infoHotspots": []
     },
     {
-      "id": "1-kitchen",
-      "name": "Kitchen",
+      "id": "2-restroom",
+      "name": "Restroom",
       "levels": [
         {
           "tileSize": 256,
@@ -69,31 +100,24 @@ var APP_DATA = {
       ],
       "faceSize": 1728,
       "initialViewParameters": {
-        "yaw": 3.0123266292568527,
-        "pitch": 0.29839752357970895,
-        "fov": 1.283603139734972
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 0.027404732436862034,
-          "pitch": 0.2179946019815251,
+          "yaw": -0.11639815987019375,
+          "pitch": 0.3744913954890521,
           "rotation": 0,
-          "target": "0-bedroom"
+          "target": "1-bedroom"
         }
       ],
-      "infoHotspots": [
-        {
-          "yaw": 0.02228016612861694,
-          "pitch": -0.15480669062339558,
-          "title": "Bedroom",
-          "text": "Text"
-        }
-      ]
+      "infoHotspots": []
     }
   ],
-  "name": "Season Park Apartment 607",
+  "name": "Kanvas Soho",
   "settings": {
-    "mouseViewMode": "drag",
+    "mouseViewMode": "qtvr",
     "autorotateEnabled": true,
     "fullscreenButton": true,
     "viewControlButtons": true
